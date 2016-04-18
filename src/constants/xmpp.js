186d2204ec3bec132231e5ae14f77f4d8f259983
Rui Ramos <rui@ometria.com>
2016-04-18 14:35:39 +0100

    build

--- src/constants/xmpp.js
+++ src/constants/xmpp.js
@@ -1,5 +1,5 @@
 module.exports = {
-  jid: 'guest@ruiramos.com',
+  jid: 'guest@chat.ruiramos.com',
   password: '', //guest, no pw
   url: 'wss://chat.ruiramos.com:5281/xmpp-websocket',
   host: '@muc.chat.ruiramos.com'
