7954ec0674a4738ed9c52e7e466c0d60e408217e
Rui Ramos <rui@ometria.com>
2016-04-18 14:20:29 +0100

    xmpp server location updated

--- src/constants/xmpp.js
+++ src/constants/xmpp.js
@@ -1,6 +1,6 @@
 module.exports = {
   jid: 'guest@ruiramos.com',
   password: '', //guest, no pw
-  url: 'wss://ruiramos.com:5281/xmpp-websocket',
+  url: 'wss://chat.ruiramos.com:5281/xmpp-websocket',
   host: '@chat.ruiramos.com'
 };
